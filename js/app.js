17673fa21dd5116704b44689da7d2eaf84239c15
saurabhpmondal <saurabhpmondal@gmail.com>
2026-03-14 10:41:41 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,41 @@
-import { renderHeader } from './ui/header/headerRenderer.js';
-import { renderSidebar } from './ui/sidebar/sidebarRenderer.js';
-import { renderFilters } from './ui/filters/filterRenderer.js';
-import { renderCards } from './ui/cards/kpiCardRenderer.js';
-import { renderDashboard } from './ui/dashboard/dashboardRenderer.js';
+// js/app.js
 
-renderHeader();
-renderSidebar();
-renderFilters();
-renderCards();
-renderDashboard();
+import { renderHeader } from "./ui/header/headerRenderer.js";
+import { renderSidebar } from "./ui/sidebar/sidebarRenderer.js";
+import { renderFilters } from "./ui/filters/filterRenderer.js";
+import { renderCards } from "./ui/cards/kpiCardRenderer.js";
+import { renderDashboard } from "./ui/dashboard/dashboardRenderer.js";
+
+import { loadAllData } from "./core/dataLoader.js";
+import { logDataSummary } from "./core/dataStore.js";
+
+async function startApp() {
+
+    console.log("Starting Flipkart Analytics App");
+
+    try {
+
+        // Load all datasets from Google Sheets
+        await loadAllData();
+
+        // Show dataset stats
+        logDataSummary();
+
+        // Render UI
+        renderHeader();
+        renderSidebar();
+        renderFilters();
+        renderCards();
+        renderDashboard();
+
+        console.log("App loaded successfully");
+
+    } catch (error) {
+
+        console.error("App failed to start:", error);
+
+    }
+
+}
+
+startApp();
