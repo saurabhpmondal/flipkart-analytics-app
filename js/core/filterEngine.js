7c3f79abc4f3efdf6fd83dea3e505900a7829c49
saurabhpmondal <saurabhpmondal@gmail.com>
2026-03-14 10:53:32 +0530

    Update filterEngine.js

--- js/core/filterEngine.js
+++ js/core/filterEngine.js
@@ -1,1 +1,46 @@
-// placeholder
+// js/core/filterEngine.js
+
+import { getData } from "./dataStore.js";
+
+let filters = {
+    startDate: null,
+    endDate: null
+};
+
+export function setDateFilter(start, end) {
+
+    filters.startDate = start;
+    filters.endDate = end;
+
+}
+
+export function getFilters() {
+
+    return filters;
+
+}
+
+export function applyDateFilter(datasetName, dateField) {
+
+    const data = getData(datasetName);
+
+    if (!filters.startDate || !filters.endDate) {
+        return data;
+    }
+
+    const start = new Date(filters.startDate);
+    const end = new Date(filters.endDate);
+
+    return data.filter(row => {
+
+        const dateValue = row[dateField];
+
+        if (!dateValue) return false;
+
+        const rowDate = new Date(dateValue);
+
+        return rowDate >= start && rowDate <= end;
+
+    });
+
+}
